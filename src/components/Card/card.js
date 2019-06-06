46b998920e5509aea2239d2a813df5e1c3191908
ArthurXG <arthurxaviergiffoni@gmail.com>
2019-06-06 00:32:54 -0300

    Alinhamento cards destaques

--- src/components/Card/card.js
+++ src/components/Card/card.js
@@ -3,14 +3,13 @@ import imageCourse from "../../assets/img/Courses/javaScrip.png"
 import './card.css'
 
 const Card = () => (
-  <div className="card card-bord card-weight">
-    <a href="#">
-      <img src={imageCourse} className="card-image-position" alt="Curso JavaScript"></img>
-      <div className="card-body">
-        <h4 className="card-title">Java Script</h4>
-        <p className="card-text">Programando na linguagem da web</p>
-      </div>
-    </a>
+  <div className="card card-bord card-weight m-1">
+    <img src={imageCourse} className="card-image-position" alt="Curso JavaScript"></img>
+    <div className="card-body">
+      <h4 className="card-title">Java Script</h4>
+      <p className="card-text">Programando na linguagem da web</p>
+      <a href="#" className="btn card-button">Acessar</a>
+    </div>
   </div>
 );
 
--- src/components/Highlights/highlights.js
+++ src/components/Highlights/highlights.js
@@ -1,51 +1,26 @@
 import React from "react";
 import Card from "./../Card/card";
 
+import "./higlights.css";
+
 const Highlights = () => (
   <div className="container">
-    <div className="row">
-      <div className="col-4">
-        <Card />
-      </div>
-      <div className="col-4">
-        <Card />
-      </div>
-      <div className="col-4">
-        <Card />
-      </div>
-      <div className="col-4">
-        <Card />
-      </div>
-      <div className="col-4">
-        <Card />
-      </div>
-      <div className="col-4">
-        <Card />
-      </div>
+    <div className="row d-flex justify-content-center">
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </div>
+    <div className="d-flex justify-content-center ">
+    <button class="btn btn-primary button-explore mt-3" type="submit">Explore todos os cursos</button>
     </div>
   </div>
-  // <div className="container justify-content-center">
-  //   <div className="row">
-  //     <div className="col-12 col-md-6 col-lg-2">
-  //       <Card/>
-  //     </div>
-  //     <div className="col-12 col-md-6 col-lg-2">
-  //       <Card/>
-  //     </div>
-  //     <div className="col-12 col-md-6 col-lg-2">
-  //       <Card/>
-  //     </div>
-  //     <div className="col-12 col-md-6 col-lg-2">
-  //       <Card/>
-  //     </div>
-  //     <div className="col-12 col-md-6 col-lg-2">
-  //       <Card/>
-  //     </div>
-  //     <div className="col-12 col-md-6 col-lg-2">
-  //       <Card/>
-  //     </div>
-  //   </div>
-  // </div>
 );
 
+/* TODO: Adicionar o card de destaque, equivalente a
+tamnho de dois cards normais */
 export default Highlights;
